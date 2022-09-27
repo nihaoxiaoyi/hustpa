bf5db4d0b02c5ebfe97cbb55840af12cf0ec89a6
tracer-ics2019 <tracer@njuics.org>
2022-09-27 17:29:13 +0800

    >  compile U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:29:13 up  2:31,  0 users,  load average: 0.02, 0.01, 0.00 2f30d73f2e2f712f97cc6273fab590a0bed29cf3

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -150,7 +150,10 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest, const rtlreg_t 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
   // TODO();
-  rtl_shr(dest,src1,width*8-1);
+  uint32_t w = width*8-1;
+  uint32_t num = 1;
+  num = num<<w;
+  *dest = ((*src1)&num)>>w;
 }
 
 static inline void rtl_mux(rtlreg_t* dest, const rtlreg_t* cond, const rtlreg_t* src1, const rtlreg_t* src2) {

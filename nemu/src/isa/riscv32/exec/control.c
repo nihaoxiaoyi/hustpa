bc0b42bcd2e606093051614488c650b7054fe555
tracer-ics2019 <tracer@njuics.org>
2022-09-27 21:49:26 +0800

    >  compile U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:49:26 up  6:51,  0 users,  load average: 0.01, 0.02, 0.00 9ad746702186f12e39b0688f5c3d8e85c16eb03

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -5,7 +5,7 @@
 make_EHelper(jal){
   uint32_t addr=cpu.pc+4;
   rtl_sr(id_dest->reg, &addr, 4);
-  printf("control.c J:rd=0x%x\n",reg_l(id_dest->reg));  // test
+  printf("control.c J:%s=0x%x\n",reg_name(id_dest->reg,4),reg_l(id_dest->reg));  // test
 
   rtl_add(&decinfo.jmp_pc, &cpu.pc, &id_src->val);
   rtl_j(decinfo.jmp_pc);

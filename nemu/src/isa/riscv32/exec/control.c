bd03bab1a9f9cedd496f611eabb4d6b3b3931730
tracer-ics2019 <tracer@njuics.org>
2022-09-27 21:15:20 +0800

    >  compile U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:15:20 up  6:17,  0 users,  load average: 0.00, 0.00, 0.00 aba3213b9aa44aa6aff66504e45c92d9df67ac

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -4,8 +4,8 @@
 /* Start */
 make_EHelper(jal){
   uint32_t addr=cpu.pc+4;
-  rtl_sr(id_dest->reg,&addr,4);
-  rtl_add(&decinfo.jmp_pc,&cpu.pc,&id_src->val);
+  rtl_sr(id_dest->reg, &addr, 4);
+  rtl_add(&decinfo.jmp_pc, &cpu.pc, &id_src->val);
   rtl_j(decinfo.jmp_pc);
 
   print_asm_template2(jal);
@@ -13,11 +13,11 @@ make_EHelper(jal){
 
 make_EHelper(jalr){
   uint32_t addr=cpu.pc+4;
-  rtl_sr(id_dest->reg,&addr,4);
-  decinfo.jmp_pc=(id_src->val+id_src2->val)&~1;
+  rtl_sr(id_dest->reg, &addr, 4);
+  decinfo.jmp_pc = (id_src->val+id_src2->val)&(~1);
   rtl_j(decinfo.jmp_pc);
 
-//   difftest_skip_dut(1, 2); //difftest
+  difftest_skip_dut(1, 2); //difftest
 
   print_asm_template2(jalr);
 }

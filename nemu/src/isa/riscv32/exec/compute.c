58701930887da7d36e75ae6456eacb7161f0d292
tracer-ics2019 <tracer@njuics.org>
2022-09-27 19:53:12 +0800

    >  compile U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:53:12 up  4:55,  0 users,  load average: 0.01, 0.01, 0.00 6464381d160a9dff275c0404ef5b91a1bc53071

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -8,7 +8,7 @@ make_EHelper(lui) {
 
 /* my instruction */
 /* Start */
-make_DHelper(auipc) {
+make_EHelper(auipc) {
   rtl_add(&id_dest->val, &cpu.pc, &id_src->val);
   rtl_sr(id_dest->reg,&id_dest->val,4);
 

35867f1f45a2b8eb46d23c3c1367e86724f1b919
tracer-ics2019 <tracer@njuics.org>
2022-10-01 12:51:22 +0800

    >  compile U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:51:22 up  1:44,  0 users,  load average: 0.08, 0.02, 0.01 ec1e6e3be77389dae2efa3d079f410738244320

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,20 +3,95 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+/* Start */
+
+static char buf[1000];
+
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsprintf(buf, fmt, ap);
+  va_end(ap);
+  int i = 0;
+  while(buf[i] != '\0'){
+    _putc(buf[i++]);
+  }
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  char *p = out;
+  while((*fmt)!='\0'){
+    if(*fmt!='%'){
+      *p++=*fmt++;
+    }
+    else{
+      fmt++;
+      switch( *fmt ){
+        case 's':{
+          char *str = va_arg(ap, char*);
+          while(*str!='\0'){
+            *p++ = *str++;
+          }
+          break;
+        }
+        case 'd':{
+          char num_buf[11];
+          int num = va_arg(ap, int);
+          if(num < 0){
+            *p++ = '-';
+          }
+          int i = 0;
+          do{
+            num_buf[i++] = num%10+'0';
+            num = num/10;
+          }while(num != 0);
+          while (--i >= 0){
+            *p++ = num_buf[i];
+          }
+          break;
+        }
+        case 'x':{
+          char buf[12];
+          uint32_t hex_num = va_arg(ap, uint32_t);
+          *p++='0';
+          *p++='x';
+          int i=0;
+          do{
+            int tmp_num = hex_num % 16;
+            if(tmp_num < 10){
+              buf[i++]=tmp_num+'0';
+            }  
+            else{
+              buf[i++] = tmp_num-10+'a';
+            }
+            hex_num = hex_num/16;
+          }while(hex_num!=0);
+          while (--i>=0){
+            *p++=buf[i];
+          }
+          break;
+        }
+      }
+      fmt++;
+    }
+  }
+  *p = '\0';
+  return p-out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap,fmt);
+  int len=vsprintf(out,fmt,ap);
+  va_end(ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
 }
+
+/* End */
 
 #endif
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -2,40 +2,100 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+/* My code */
+/* Start */
+
 size_t strlen(const char *s) {
-  return 0;
+  assert(s!=NULL);
+  size_t len = 0;
+  while ( s[len++] != '\0' );
+  return len-1;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  assert(dst!=NULL && src!=NULL);
+  size_t i = 0;
+  while (src[i]!='\0') {
+    dst[i]=src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  assert(dst!=NULL && src!=NULL);
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++){
+    dst[i] = src[i];
+  }
+  while (i < n){
+    dst[i++] = '\0';
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  assert(dst!=NULL && src!=NULL);
+  size_t dst_len = strlen(dst);
+  size_t i;
+  for (i = 0 ; src[i] != '\0' ; i++){
+    dst[dst_len + i] = src[i];
+  }
+  dst[dst_len + i] = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  assert(s1!=NULL && s2!=NULL);
+  size_t i = 0;
+  while (s1[i] && s2[i] && s1[i]==s2[i]){
+    i++;
+  }
+  return s1[i]-s2[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  assert(s1!=NULL && s2!=NULL);
+  if(n < 1) return 0;
+  size_t i = 0;
+  while (i<n && s1[i] && s2[i] && s1[i]==s2[i]){
+    i++;
+  }
+  return s1[i]-s2[i];
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  assert(v!=NULL);
+  char *p = (char* )v;
+  for (size_t i = 0; i < n; i++){
+    p[i]=(char)c;
+  }
+  return p;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  assert(out!=NULL && in!=NULL);
+  char* p = (char *)out;
+  char* q = (char *)in;
+  while (n-->0){
+    *p++ = *q++;
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  assert(s1!=NULL && s2!=NULL);
+  if(n < 1) return 0;
+  char* p = (char *)s1;
+  char* q = (char *)s2;
+  while (*p && *q && n-->0){
+    p++;
+    q++; 
+  }
+  return *p - *q;
 }
+
+/* End */
 
 #endif

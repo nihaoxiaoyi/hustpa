004096032437d37a7992217806c8384b17d38807
tracer-ics2019 <tracer@njuics.org>
2022-10-09 15:16:31 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:16:31 up  1:39,  0 users,  load average: 0.08, 0.05, 0.01 2e839a6d51ba41776ed6a37eb4fd48d73c3afc8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,14 +14,12 @@
 #include<fs.h>
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
-// size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
-// int fs_close(int fd);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 /* End */
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // TODO();
   /* Start */
 
   Elf_Ehdr head;

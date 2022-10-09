8a5a542a595fee12ac7b0f791eaa70ed947402c5
tracer-ics2019 <tracer@njuics.org>
2022-10-09 16:39:38 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:39:38 up  3:02,  1 user,  load average: 0.29, 0.43, 0.21 6ca5c6408db2f02e110c496d362de317f0f26229

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,4 +37,9 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  /* Start */
+
+  sprintf(dispinfo, "WIDTH:%d\n HEIGHT:%d\n", screen_width(), screen_height() );
+
+  /* End */
 }

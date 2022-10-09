10663a1b3d4fe0f366911f43f166c49c96fc7ebc
tracer-ics2019 <tracer@njuics.org>
2022-10-09 15:35:50 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:35:50 up  1:58,  1 user,  load average: 0.16, 0.39, 0.21 c12fbb3aa6f93b6e7437af2704af48d108419fe

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -1,40 +1,3 @@
-// #include "proc.h"
-
-// #define MAX_NR_PROC 4
-
-// static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
-// static PCB pcb_boot = {};
-// PCB *current = NULL;
-
-// void switch_boot_pcb() {
-//   current = &pcb_boot;
-// }
-
-// void hello_fun(void *arg) {
-//   int j = 1;
-//   while (1) {
-//     Log("Hello World from Nanos-lite for the %dth time!", j);
-//     j ++;
-//     _yield();
-//   }
-// }
-
-// void init_proc() {
-//   switch_boot_pcb();
-
-//   Log("Initializing processes...");
-
-//   // load program here
-//   /* Start */
-
-//   naive_uload(NULL, "/bin/text");  // NULL "/bin/bal"
-
-//   /* End */
-// }
-
-// _Context* schedule(_Context *prev) {
-//   return NULL;
-// }
 #include "proc.h"
 
 #define MAX_NR_PROC 4
@@ -60,8 +23,13 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/dummy");
 
+  // load program here
+  /* Start */
+
+  naive_uload(NULL, "/bin/text");  // NULL "/bin/bal"
+
+  /* End */
 }
 
 _Context* schedule(_Context *prev) {

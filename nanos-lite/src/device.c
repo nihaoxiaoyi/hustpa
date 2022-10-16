769e780791c446981a13379b7f6f87ea30aec4d3
tracer-ics2019 <tracer@njuics.org>
2022-10-16 21:09:36 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:09:36 up  6:33,  1 user,  load average: 0.13, 0.28, 0.42 dc7e3ef28bde4bb416b9957f37b8a2499486d6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,21 +1,107 @@
+// #include "common.h"
+// #include <amdev.h>
+// /* Start */
+
+// // #include<klib.h>
+
+// /* End*/
+
+// size_t serial_write(const void *buf, size_t offset, size_t len) {
+//   /* Start */
+
+//   len = len>0 ? len : 0;
+//   for(int i=0; i<len; i++) {
+//     _putc(((char *)buf)[i]);
+//   }
+//   return len;
+
+//   /* End */
+// }
+
+// #define NAME(key) \
+//   [_KEY_##key] = #key,
+
+// static const char *keyname[256] __attribute__((used)) = {
+//   [_KEY_NONE] = "NONE",
+//   _KEYS(NAME)
+// };
+
+// /* Start */
+
+// #define KEYDOWN_MASK 0x8000
+
+// /* End */
+
+// size_t events_read(void *buf, size_t offset, size_t len) {
+//   /* Start */
+  
+//   int key = read_key();
+//   bool down = false;
+//   if( key & KEYDOWN_MASK ){
+//     key ^= 0x8000;
+//     down = true;
+//   }
+//   if(key){
+//     len = sprintf(buf,"%s %s\n", down ?"kd":"ku", keyname[key]);
+//   }
+//   else{
+//     len = sprintf(buf,"t %d\n", uptime());
+//   }
+//   return len;
+
+//   /* End */
+// }
+
+// static char dispinfo[128] __attribute__((used)) = {};
+
+// size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+//   /* Start */
+  
+//   return sprintf(buf, dispinfo+offset);
+
+//   /* End */
+// }
+
+// size_t fb_write(const void *buf, size_t offset, size_t len) {
+//   /* Start */
+  
+//   int x = (offset/4) % screen_width();
+//   int y = (offset/4) / screen_width();
+//   draw_rect((uint32_t*)buf, x, y, len / 4, 1);
+//   return len;
+
+//   /* End */
+// }
+
+// size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+//   /* Start */
+  
+//   draw_sync();
+//   return len;
+
+//   /* End */
+// }
+
+// void init_device() {
+//   Log("Initializing devices...");
+//   _ioe_init();
+
+//   // TODO: print the string to array `dispinfo` with the format
+//   // described in the Navy-apps convention
+//   /* Start */
+
+//   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height() );
+
+//   /* End */
+// }
 #include "common.h"
 #include <amdev.h>
-/* Start */
 
-#include<klib.h>
-
-/* End*/
-
+// implement some function of device
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  /* Start */
-
-  len = len>0 ? len : 0;
-  for(int i=0; i<len; i++) {
+  for(int i=0;i<len;i++)
     _putc(((char *)buf)[i]);
-  }
   return len;
-
-  /* End */
 }
 
 #define NAME(key) \
@@ -26,71 +112,44 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
-/* Start */
-
-#define KEYDOWN_MASK 0x8000
-
-/* End */
-
-size_t events_read(void *buf, size_t offset, size_t len) {
-  /* Start */
-  
-  int key = read_key();
-  bool down = false;
-  if( key & KEYDOWN_MASK ){
-    key ^= 0x8000;
-    down = true;
+size_t events_read(void *buf, size_t offset, size_t len) {  
+  int key=read_key();
+  int down=0;
+  if(key&0x8000){
+    key^=0x8000;
+    down=1;
   }
-  if(key){
-    len = sprintf(buf,"%s %s\n", down ?"kd":"ku", keyname[key]);
+  if(key!=_KEY_NONE){
+    len=sprintf(buf,"%s %s\n",down ?"kd":"ku",keyname[key]);
   }
   else{
-    len = sprintf(buf,"t %d\n", uptime());
+    int time=uptime();
+    len=sprintf(buf,"t %d\n",time);
   }
   return len;
-
-  /* End */
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  /* Start */
-  
-  return sprintf(buf, dispinfo+offset);
-
-  /* End */
+  len=sprintf(buf,dispinfo+offset);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  /* Start */
-  
-  int x = (offset/4) % screen_width();
-  int y = (offset/4) / screen_width();
+  int x=(offset/4)%screen_width();
+  int y=(offset/4)/screen_width();
   draw_rect((uint32_t*)buf, x, y, len / 4, 1);
   return len;
-
-  /* End */
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  /* Start */
-  
   draw_sync();
   return len;
-
-  /* End */
 }
 
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
-  /* Start */
-
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height() );
-
-  /* End */
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",screen_width(),screen_height());
 }

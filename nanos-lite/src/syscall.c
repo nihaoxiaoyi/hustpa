04e6037db0a4f6a30ac3dbb76d445c02238abf11
tracer-ics2019 <tracer@njuics.org>
2022-10-17 21:34:57 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:34:57 up  8:03,  1 user,  load average: 0.10, 0.13, 0.06 a7a01879e2f8058a138bd3b780d96987c12ed6f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,11 +3,14 @@
 
 /* Start */
 
+#include "proc.h"
+
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+void naive_uload(PCB *pcb, const char *filename);
 
 /* End */
 
@@ -27,14 +30,15 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     /* Start */
     
-    case SYS_yield: _yield(); break;
-    case SYS_exit : _halt(a[1]); break;
-    case SYS_write: ret = fs_write(a[1],(void*)a[2],a[3]); break;
-    case SYS_read : ret = fs_read(a[1],(void*)a[2],a[3]); break;
-    case SYS_open : ret = fs_open((char*)a[1],a[2],a[3]); break;
-    case SYS_close: ret = fs_close(a[1]); break;
-    case SYS_lseek: ret = fs_lseek(a[1],a[2],a[3]); break;
-    case SYS_brk  : break;
+    case SYS_yield : _yield(); break;
+    case SYS_exit  : _halt(a[1]); break;
+    case SYS_write : ret = fs_write(a[1],(void*)a[2],a[3]); break;
+    case SYS_read  : ret = fs_read(a[1],(void*)a[2],a[3]); break;
+    case SYS_open  : ret = fs_open((char*)a[1],a[2],a[3]); break;
+    case SYS_close : ret = fs_close(a[1]); break;
+    case SYS_lseek : ret = fs_lseek(a[1],a[2],a[3]); break;
+    case SYS_brk   : break;
+    case SYS_execve: naive_uload(NULL, a[1]); break;
     
     /* End */
     default: panic("Unhandled syscall ID = %d", a[0]);

862af02bf6f891436822389f4921f2effa4a678a
tracer-ics2019 <tracer@njuics.org>
2022-10-17 21:57:41 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:57:41 up  8:26,  1 user,  load average: 0.17, 0.54, 0.59 cceda7b1bd69d47bb3e171c4c245c9a1e3db030

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,10 +1,5 @@
 #include "common.h"
 #include <amdev.h>
-/* Start */
-
-// #include<klib.h>
-
-/* End*/
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   /* Start */
@@ -67,7 +62,6 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   
   int x = (offset/4) % screen_width();
   int y = (offset/4) / screen_width();
-  // draw_sync();
   draw_rect((uint32_t*)buf, x, y, len / 4, 1);
   return len;
 

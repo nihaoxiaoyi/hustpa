9fbb0dbf049241c616747007b2bcd4351fa5ec86
tracer-ics2019 <tracer@njuics.org>
2022-10-18 17:09:10 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:09:10 up  3:40,  1 user,  load average: 0.12, 0.06, 0.01 a0113505ef11c1cde599adee338d46247437410

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -41,7 +41,7 @@
 /* Start */
 
 extern char _end;
-// int _halt(int );
+extern int _halt(int );
 
 /* End */
 
@@ -58,7 +58,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 void _exit(int status) {
   _syscall_(SYS_exit, status, 0, 0);
   // while (1);
-  // _halt(status);
+  _halt(status);
 }
 
 int _open(const char *path, int flags, mode_t mode) {

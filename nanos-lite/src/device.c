580dc160ad5a5dfb034ea2b9dde047ef24c9f9ed
tracer-ics2019 <tracer@njuics.org>
2022-10-18 19:10:06 +0800

    >  run U201914942 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:10:06 up  5:41,  1 user,  load average: 0.29, 0.16, 0.06 ae1298c546c2a35c35af24dcd5c892e26d195143

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,6 +38,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     down = true;
   }
   if(key){
+    printf("key=%d\n",key);
     len = sprintf(buf,"%s %s\n", down ?"kd":"ku", keyname[key]);
   }
   // else{
